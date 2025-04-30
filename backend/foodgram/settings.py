abe2f7caea520548bf427f494ea3f46d4bd572ca
Anton K <executor1@gmail.com>
2025-04-30 03:22:52 +0300

    modify key

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='token')
+# SECRET_KEY = os.getenv('SECRET_KEY', default='token')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
@@ -29,9 +30,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
-    'apps.accounts.apps.AccountsConfig',
     'apps.api.apps.ApiConfig',
     'apps.recipe.apps.RecipeConfig',
+    'apps.accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
