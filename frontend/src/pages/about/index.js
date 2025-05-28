5337b91011ec9717c8af7303f1a6b23b045f13fd
Anton K <executor1@gmail.com>
2025-05-29 02:37:59 +0300

    uncomment about and technologies_4

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/AntonPyth/foodgram" className={styles.textLink}>Страница автора на Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/AntonPyth" className={styles.textLink}>Антон</a>
             </p>
           </div>
         </aside>
