5319be360c540d73027ec0854a667d0eb3e18609
Anton K <executor1@gmail.com>
2025-08-12 00:03:27 +0300

    hiding SECRET_KEY

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,18 +1,15 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECRET_KEY = os.getenv('SECRET_KEY', default='token')
-
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
-
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
-#                          default='127.0.0.1,localhost').split(',')
 
 domain_name = os.getenv('ALLOWED_HOSTS')
 if domain_name:
@@ -137,7 +134,6 @@ DJOSER = {
         'token': 'apps.api.views.CustomTokenCreateView',
     },
 }
-
 
 # project constants
 MAX_LENGTH_EMAIL = 254
