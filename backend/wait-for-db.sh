ae9ee5794b2e4a9d1d3bde598b66b90142c9c640
Anton K <executor1@gmail.com>
2025-08-11 04:21:55 +0300

    fixing bug_5

--- backend/wait-for-db.sh
+++ backend/wait-for-db.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-
+#!/bin/sh
 set -e
 
 host="$1"
-shift
+port="$2"
+shift 2
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+echo "⏳ Ожидание базы данных на ${host}:${port}..."
+until nc -z "$host" "$port"; do
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd 
+echo "✅ База данных доступна, запускаем: $cmd"
+exec $cmd
