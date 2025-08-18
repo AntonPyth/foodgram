2da8893929303b56432359207cb301f6fca4d0f7
Anton K <executor1@gmail.com>
2025-08-19 00:53:20 +0300

    fixing_review_6

--- backend/apps/api/serializers.py
+++ backend/apps/api/serializers.py
@@ -1,5 +1,4 @@
 from drf_extra_fields.fields import Base64ImageField
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
