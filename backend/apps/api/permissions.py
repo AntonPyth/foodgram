f551c721f8d69f4aa889e107b17803fe60db458a
Anton K <executor1@gmail.com>
2025-08-21 03:39:39 +0300

    SAFE_METHODS

--- backend/apps/api/permissions.py
+++ backend/apps/api/permissions.py
@@ -1,11 +1,11 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwnerOrReadOnly(BasePermission):
     """Разрешение на чтение для всех и изменение только владельцу."""
 
     def has_object_permission(self, request, view, obj):
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.method in SAFE_METHODS:
             return True
         owner = getattr(obj, 'author', None) or getattr(obj, 'user', None)
         return owner == request.user
